fde927cc4e9025fa836d7d8c52dc5dcbdedd946e
Fausto Morales <fausto@robinbay.com>
2025-02-27 23:24:23 -0500

    Add a circle example.

--- src/routes/exercises/sphiro/v0/+page.svelte
+++ src/routes/exercises/sphiro/v0/+page.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
     import Exercise from "$lib/components/Exercise.svelte";
-    import Step0 from "./0_Introduction.svelte"
+    import Step0 from "./0_Introduction.svelte";
+
+    var circle = `
+    var driveInCircles = async (sphero) => {
+    for (var circle=0; circle < 2; circle++) {
+        for (var heading=0; heading < 360; heading += 10) {
+        console.log(\`[DRIVE]: circle: \${circle}, heading: \${heading}\`)
+        await sphero.roll(100, heading)
+        await sphero.delay(25)
+        }
+    }
+    sphero.roll(0, 0)
+    }
+    `;
 </script>
 
 <Exercise
