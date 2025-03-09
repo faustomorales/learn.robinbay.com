27aeff0fc28d542b5fa8a32fbaa055a24b81af2f
Fausto Morales <fausto@robinbay.com>
2025-03-09 13:54:26 -0400

    Working version of AnnotatedCode element.

--- src/lib/components/AnnotatedCode.svelte
+++ src/lib/components/AnnotatedCode.svelte
@@ -4,8 +4,11 @@
     import Prism from "prismjs";
     import "prismjs/plugins/keep-markup/prism-keep-markup";
 
-    let props: { language: "markup" | "css" | "javascript"; code: string; class?: ClassValue } =
-        $props();
+    let props: {
+        language: "markup" | "css" | "javascript";
+        code: string;
+        class?: ClassValue;
+    } = $props();
     let sanitized =
         props.language === "markup"
             ? props.code
@@ -20,7 +23,7 @@
     onMount(() => Prism.highlightAllUnder(container));
 </script>
 
-<div class={`bg-gray-800 rounded-lg p-4 ${props.class}`} bind:this={container}>
+<div class={`bg-gray-800 dark:bg-gray-1000 rounded-lg p-4 ${props.class}`} bind:this={container}>
     <pre class="!bg-transparent"><code class={`language-${props.language}`}
             >{@html sanitized}</code
         ></pre>
@@ -34,6 +37,20 @@
         color: unset;
     }
 
+    :global(mark:hover:after) {
+        content: attr(data-value);
+        padding: 4px 8px;
+        color: black;
+        background-color: white;
+        border: 1px black solid;
+        box-shadow: 0 0 3px #ccc;
+
+        position: absolute;
+        left: 0;
+        top: 100%;
+        z-index: 10;
+    }
+
     :global(mark:hover) {
         background-color: rgba(255, 255, 0, 0.2);
     }
@@ -43,5 +60,6 @@
         margin: 0;
         padding: 0;
         overflow-x: auto;
+        overflow: visible;
     }
 </style>
